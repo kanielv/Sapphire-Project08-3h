fb5062f216c76b3df9104f3ff7abd260b81533dd
JacobLevine04 <108999054+JacobLevine04@users.noreply.github.com>
2023-11-06 16:44:11 -0500

    This is the inital login page with button

---
+++ client/src/views/GoogleAuthLogin/GoogleAuthLogin.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GoogleAuthLogin = () => {
+  return (
+    <div style={{ background: 'white', minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <button>This will be the google login button</button>
+    </div>
+  );
+};
+
+export default GoogleAuthLogin;
