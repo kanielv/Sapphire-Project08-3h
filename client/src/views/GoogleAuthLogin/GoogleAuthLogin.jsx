f50d175411d30eb8a0a6ec3176295107d316e00a
Kaniel Vicencio <kanielvicencio@gmail.com>
2023-11-14 01:26:26 -0500

    completed working google button

--- client/src/views/GoogleAuthLogin/GoogleAuthLogin.jsx
+++ client/src/views/GoogleAuthLogin/GoogleAuthLogin.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
+import { GoogleLogin } from '@react-oauth/google';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 const GoogleAuthLogin = () => {
+
   return (
-    <div style={{ background: 'white', minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <button>This will be the google login button</button>
-    </div>
+    <div style={{ background: 'white', minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }} >
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_CLIENT_ID} >
+        <GoogleLogin onSuccess={credentialRes => { console.log(credentialRes) }} onError={() => console.log("Login Failed")} />
+      </GoogleOAuthProvider>
+    </div >
+
   );
 };
 
