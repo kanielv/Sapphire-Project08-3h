5f52c6f173738a0fb49f9869f8662c224474637b
Kaniel Vicencio <kanielvicencio@gmail.com>
2023-11-26 21:27:11 -0500

    add working redirct url to Google Sign In

--- client/src/views/GoogleAuthLogin/GoogleAuthLogin.jsx
+++ client/src/views/GoogleAuthLogin/GoogleAuthLogin.jsx
@@ -3,17 +3,20 @@ import React, { useEffect, useState } from 'react';
 import { GoogleLogin } from '@react-oauth/google';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import { getGoogleLoginUrl } from '../../Utils/GoogleAuthRequests';
+import { useLinkClickHandler } from 'react-router-dom';
+
 
 const GoogleAuthLogin = () => {
 
   const handleLoginCallback = async () => {
     const url = await getGoogleLoginUrl()
-    console.log(url)
+    console.log(window.location.replace(url.data.url))
+    // window.location.replace(url)
   }
 
   return (
     <div style={{ background: 'white', minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }} >
-      <button onClick={handleLoginCallback}></button>
+      <button onClick={handleLoginCallback}> Sign In With Google </button>
     </div >
 
   );
